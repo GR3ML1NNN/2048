b7db9ed3b0e8a80f5d700305f68ee0e5bc2e10d1
GR3ML1NNN <tye77166@gmail.com>
2025-06-03 16:21:54 -0400

    more efficient

--- main.py
+++ main.py
@@ -9,25 +9,26 @@ clock = pg.time.Clock()
 screenWidth, screenHeight = pg.display.get_surface().get_size()
 font = pg.font.SysFont('Arial', 60)
 
-grid =  [[0, 0, 0, 0],
-         [0, 0, 0, 0],
-         [0, 0, 0, 0],
-         [0, 0, 0, 0]]
+grid = [[0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]]
 
 colors = {
-        0: (144, 140, 170),
-        2: (156, 207, 216),
-        4: (62, 143, 176),
-        8: (86, 148, 159),
-        16: (40, 105, 131),
-        32: (196, 167, 231),
-        64: (144, 122, 169),
-        128: (246, 193, 119),
-        256: (234, 154, 151),
-        512: (235, 111, 146),
-        1024: (235, 111, 146),
-        2048: (180, 99, 122)
+    0: (144, 140, 170),
+    2: (156, 207, 216),
+    4: (62, 143, 176),
+    8: (86, 148, 159),
+    16: (40, 105, 131),
+    32: (196, 167, 231),
+    64: (144, 122, 169),
+    128: (246, 193, 119),
+    256: (234, 154, 151),
+    512: (235, 111, 146),
+    1024: (235, 111, 146),
+    2048: (180, 99, 122)
 }
+
 
 def drawGrid(grid):
     global screenWidth, screenHeight
@@ -47,13 +48,15 @@ def drawGrid(grid):
 
             color = colors.get(j)
             pg.draw.rect(screen, color,
-                         (x + 5, y + 5, recWidth - 10, recLength - 10), border_radius = 8)
+                         (x + 5, y + 5, recWidth - 10, recLength - 10), border_radius=8)
 
-            screen.blit(font.render(str(j), True, (224, 222, 244)), 
-                        (x + (recWidth // 2 - 20), y + (recLength // 2 - 30)))
+            screen.blit(font.render(str(j), True, (224, 222, 244)),
+                        (x + (recWidth // 2 - 20),
+                         y + (recLength // 2 - 30)))
 
             x += recWidth
             rowCount += 1
+
 
 def left(grid):
 
@@ -69,6 +72,7 @@ def left(grid):
     shiftLeft(grid)
     return grid
 
+
 def right(grid):
     shiftRight(grid)
 
@@ -82,13 +86,15 @@ def right(grid):
     shiftRight(grid)
     return grid
 
+
 def up(grid):
-    g = rotate(grid)
-    g = left(g)
-    g = rotate(g)
-    g = rotate(g)
-    g = rotate(g)
-    return g
+    grid = rotate(grid)
+    grid = left(grid)
+    grid = rotate(grid)
+    grid = rotate(grid)
+    grid = rotate(grid)
+    return grid
+
 
 def down(grid):
     g = rotate(grid)
@@ -98,6 +104,7 @@ def down(grid):
     g = rotate(g)
     g = rotate(g)
     return g
+
 
 def shiftLeft(grid):
     for i in range(4):
@@ -110,6 +117,7 @@ def shiftLeft(grid):
         grid[i] = nums
         grid[i].extend([0] * (4 - count))
 
+
 def shiftRight(grid):
     for i in range(4):
         nums = []
@@ -121,50 +129,55 @@ def shiftRight(grid):
         grid[i] = [0] * (4 - count)
         grid[i].extend(nums)
 
+
 def rotate(grid):
-    g = [[grid[j][i] for j in range(4)] for i in range(3, -1, -1)]
-    return g
+    grid = [[grid[j][i] for j in range(4)] for i in range(3, -1, -1)]
+    return grid
+
 
 def randomSquare(grid):
-    i = random.randint(0,3)
-    j = random.randint(0,3)
+    i = random.randint(0, 3)
+    j = random.randint(0, 3)
     flatGrid = [cell for row in grid for cell in row]
     while grid[i][j] != 0 and 0 in flatGrid:
-        i = random.randint(0,3)
-        j = random.randint(0,3)
+        i = random.randint(0, 3)
+        j = random.randint(0, 3)
         if 0 not in flatGrid:
             break
     if 0 in flatGrid:
         grid[i][j] = 2
 
+
 def checkState(grid):
-    flatGrid = [cell for row in grid for cell in row]
     for i in range(4):
         for j in range(4):
             if j != 3 and grid[i][j] == grid[i][j + 1] or \
-                i != 3 and grid[i][j] == grid[i + 1][j]:
-                    return False
+                    i != 3 and grid[i][j] == grid[i + 1][j]:
+                return False
 
+    flatGrid = [cell for row in grid for cell in row]
     if 0 not in flatGrid:
         return True
     else:
         return False
+
 
 def endScreen(win, lose):
     global screenWidth, screenHeight
     global font
     s = pg.Surface((screenWidth, screenHeight), pg.SRCALPHA)
     s.set_alpha(128)
-    s.fill((0,0,0))
+    s.fill((0, 0, 0))
     screen.blit(s, (0, 0))
 
     if win:
         msg = "You Win"
     if lose:
-        msg = "Loser!"
+        msg = "Maybe Next time..."
 
-    screen.blit(font.render(msg, True, (224, 222, 244)), 
+    screen.blit(font.render(msg, True, (224, 222, 244)),
                 (screenWidth // 3, screenHeight // 3))
+
 
 def main():
     global grid
@@ -207,11 +220,12 @@ def main():
 
         lose = checkState(grid)
         pg.display.flip()
-        clock.tick(10)
+        clock.tick(8)
 
     pg.display.quit()
     pg.quit()
     sys.exit()
+
 
 if __name__ == "__main__":
     main()
